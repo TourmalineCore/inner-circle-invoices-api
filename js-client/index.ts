75ea54e34ae498fe846667ba2323aa2aa7117e85
Workflow Action <contact@tourmalinecore.com>
2026-04-06 09:37:25 +0000

    chore(js-client): Update js-client by OpenAPI

--- js-client/index.ts
+++ js-client/index.ts
@@ -209,7 +209,7 @@ export class HttpClient<SecurityDataType = unknown> {
 
 /**
  * @title inner-circle-invoices-api
- * @version 1.1.1
+ * @version 1.1.2
  * @baseUrl http://localhost:6508/
  */
 export class Api<

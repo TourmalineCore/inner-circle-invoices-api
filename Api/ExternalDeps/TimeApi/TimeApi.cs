9e5402e8fd6a7aa2c92dbd4f8ea03cf16b9ea7a3
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2026-04-06 10:10:34 +0500

    format: #12: fix indent

--- Api/ExternalDeps/TimeApi/TimeApi.cs
+++ Api/ExternalDeps/TimeApi/TimeApi.cs
@@ -5,48 +5,48 @@ namespace Api.ExternalDeps.TimeApi;
 
 public class TimeApi : ITimeApi
 {
-  private readonly ExternalDepsUrls _externalDepsUrls;
+    private readonly ExternalDepsUrls _externalDepsUrls;
 
-  private readonly AuthenticatedHttpClient _authenticatedHttpClient;
+    private readonly AuthenticatedHttpClient _authenticatedHttpClient;
 
-  public TimeApi(
-      IOptions<ExternalDepsUrls> externalDepsUrls,
-      AuthenticatedHttpClient authenticatedHttpClient
-  )
-  {
-    _externalDepsUrls = externalDepsUrls.Value;
-    _authenticatedHttpClient = authenticatedHttpClient;
-  }
+    public TimeApi(
+        IOptions<ExternalDepsUrls> externalDepsUrls,
+        AuthenticatedHttpClient authenticatedHttpClient
+    )
+    {
+        _externalDepsUrls = externalDepsUrls.Value;
+        _authenticatedHttpClient = authenticatedHttpClient;
+    }
 
-  public async Task<TimeGetAllProjectsResponse> GetAllProjectsAsync()
-  {
-    var link = $"{_externalDepsUrls.TimeApiRootUrl}/internal/projects";
+    public async Task<TimeGetAllProjectsResponse> GetAllProjectsAsync()
+    {
+        var link = $"{_externalDepsUrls.TimeApiRootUrl}/internal/projects";
 
-    var projects = await _authenticatedHttpClient.GetAsync<TimeGetAllProjectsResponse>(link);
+        var projects = await _authenticatedHttpClient.GetAsync<TimeGetAllProjectsResponse>(link);
 
-    return projects!;
-  }
+        return projects!;
+    }
 
-  public async Task<TimeGetEmployeesTrackedTaskHoursResponse> GetEmployeesTrackedTaskHoursAsync(
-      long projectId,
-      int year,
-      int month
-  )
-  {
-    var startDate = DateOnly.Parse($"{year}-{month}-01");
+    public async Task<TimeGetEmployeesTrackedTaskHoursResponse> GetEmployeesTrackedTaskHoursAsync(
+        long projectId,
+        int year,
+        int month
+    )
+    {
+        var startDate = DateOnly.Parse($"{year}-{month}-01");
 
-    var lastDayOfMonth = DateTime.DaysInMonth(year, month);
+        var lastDayOfMonth = DateTime.DaysInMonth(year, month);
 
-    var endDate = DateOnly.Parse($"{year}-{month}-{lastDayOfMonth}");
+        var endDate = DateOnly.Parse($"{year}-{month}-{lastDayOfMonth}");
 
-    var formattedStartDate = startDate.ToString("yyyy-MM-dd");
+        var formattedStartDate = startDate.ToString("yyyy-MM-dd");
 
-    var formattedEndDate = endDate.ToString("yyyy-MM-dd");
+        var formattedEndDate = endDate.ToString("yyyy-MM-dd");
 
-    var link = $"{_externalDepsUrls.TimeApiRootUrl}/internal/projects/tracked-task-hours?projectId={projectId}&startDate={formattedStartDate}&endDate={formattedEndDate}";
+        var link = $"{_externalDepsUrls.TimeApiRootUrl}/internal/projects/tracked-task-hours?projectId={projectId}&startDate={formattedStartDate}&endDate={formattedEndDate}";
 
-    var employeesTrackedTaskHoursEntries = await _authenticatedHttpClient.GetAsync<TimeGetEmployeesTrackedTaskHoursResponse>(link);
+        var employeesTrackedTaskHoursEntries = await _authenticatedHttpClient.GetAsync<TimeGetEmployeesTrackedTaskHoursResponse>(link);
 
-    return employeesTrackedTaskHoursEntries!;
-  }
+        return employeesTrackedTaskHoursEntries!;
+    }
 }

558e6830f9e1cd5e15e6d6280a4f8cbaf63a2042
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2026-04-06 10:20:40 +0500

    fix: #12: add AuthenticatedHttpClient in DependencyInjection

--- Api/DependencyInjection.cs
+++ Api/DependencyInjection.cs
@@ -20,6 +20,7 @@ public static class DependencyInjection
 
         services.Configure<ExternalDepsUrls>(configuration.GetSection(nameof(ExternalDepsUrls)));
 
+        services.AddTransient<AuthenticatedHttpClient>();
         services.AddTransient<ITimeApi, TimeApi>();
         services.AddTransient<IEmployeesApi, EmployeesApi>();
 
